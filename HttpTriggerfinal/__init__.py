55639a89215e3b1fa5294d68796c38cf627fc6f7
emilie-lb <62951146+emilie-lb@users.noreply.github.com>
2021-04-06 15:57:33 +0200

    mise à jour fichier init httpTrigger

--- HttpTriggerfinal/__init__.py
+++ HttpTriggerfinal/__init__.py
@@ -1,49 +1,35 @@
 import logging
-import mysql.connector
 import azure.functions as func
+import mysql.connector
 import os
+import json
 
 
-
-config = {
+config_sql = {
   'host': os.environ['hostlibrary'],
   'user': os.environ['userlibrary'],
   'password': os.environ['passwordlibrary'],
-  'database':'library',
+  'database':os.environ['library'],
   'client_flags': [mysql.connector.ClientFlag.SSL],
   'ssl_ca': os.environ['sslibrary']
 }
 
 
-
 def listelivre():
-    logging.info('connection à la base de données')
-    conn = mysql.connector.connect(**config)
+    logging.debug("connection à la base de données")
+    conn = mysql.connector.connect(**config_sql)
     cursor = conn.cursor()
     cursor.execute("""
     SELECT titre FROM liste_livres
     """)
-    logging.info("la connection c'est bien passer!")
-    # res1 =[]
-    # result = cursor.fetchall()
-    # res =[]
-    # for row in result:
-    #     res.append(row["titre"])
-    # for row in cursor.fetchall():
-    #     res1.append(row)
-    # cursor.execute("SELECT titre FROM liste_livres;")
+    logging.info("la connection s'est bien passée!")
     rows = cursor.fetchall()
     liste_rows = []
     for row in rows :
         liste_rows += row
-    # res2=[]
-    # for titre in res1 :
-    #     titre= " ".join(titre)
-    #     res2.append(titre)
-    # return " ".join(res1).replace(" ",", ")
-    
+    logging.debug(f"liste livre {liste_rows}")
     return liste_rows
-# print(listelivre())
+
 
 def liste_html():
     logging.info('Regarde si livre existe ou pas pour créer liste.')
@@ -58,6 +44,7 @@ def liste_html():
             html_liste_livres += "</span>"
     return html_liste_livres
 
+
 def result_html():
     logging.info('Sort info en HTML.')
     result="""<!DOCTYPE html>
@@ -69,7 +56,7 @@ def result_html():
         <center>
         <title>library</title>
         </head>
-        <h3>Voici la liste des livres présents dans la base de donnée</h3>
+        <h3>Voici la liste des livres présents dans la base de données</h3>
         <body>
         <style>
         ul
@@ -85,31 +72,10 @@ def result_html():
         </center>
         </html>"""
     return result
-    
 
 
-# def main(req: func.HttpRequest) -> func.HttpResponse:
-#     logging.info('Python HTTP trigger function processed a request.')
-
-#     name = req.params.get('name')
-#     if not name:
-#         try:
-#             req_body = req.get_json()
-#         except ValueError:
-#             pass
-#         else:
-#             name = req_body.get('name')
-
-#     if name:
-#         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-#     else:
-#         return func.HttpResponse(
-#              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-#              status_code=200
-#         )
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     return func.HttpResponse(
         result_html(), status_code=200, mimetype='text/html'
     )
-
