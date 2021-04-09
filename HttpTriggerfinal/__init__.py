2c0a5b58379ba61048b6c3897c392053f7fe0283
StephanieR34 <77360915+StephanieR34@users.noreply.github.com>
2021-04-09 14:18:46 +0200

    mise à jour init

--- HttpTriggerfinal/__init__.py
+++ HttpTriggerfinal/__init__.py
@@ -1,28 +1,31 @@
 import logging
-import azure.functions as func
 import mysql.connector
+import azure.functions as func
 import os
-import json
+from jinja2 import Template
 
 
-config_sql = {
+config = {
   'host': os.environ['hostlibrary'],
   'user': os.environ['userlibrary'],
   'password': os.environ['passwordlibrary'],
-  'database':os.environ['library'],
+  'database': 'library',
   'client_flags': [mysql.connector.ClientFlag.SSL],
   'ssl_ca': os.environ['sslibrary']
 }
 
 
 def listelivre():
-    logging.debug("connection à la base de données")
-    conn = mysql.connector.connect(**config_sql)
+    """ Cette fonction permet de se connecter à la base de
+        données et de récupérer la colonne livre.
+    """
+    logging.debug("connexion à la base de donnée")
+    conn = mysql.connector.connect(**config)
     cursor = conn.cursor()
     cursor.execute("""
     SELECT titre FROM liste_livres
     """)
-    logging.info("la connection s'est bien passée!")
+    logging.info("la connexion OK")
     rows = cursor.fetchall()
     liste_rows = []
     for row in rows :
@@ -31,51 +34,14 @@ def listelivre():
     return liste_rows
 
 
-def liste_html():
-    logging.info('Regarde si livre existe ou pas pour créer liste.')
-    livres = listelivre()
-    html_liste_livres = """<span class="Liste livre">"""
-    for livre in livres :
-        if livre != livres[-1]:
-            html_liste_livres += livre
-            html_liste_livres += """</span><br> <span class="Liste livres">"""
-        else:
-            html_liste_livres += livre
-            html_liste_livres += "</span>"
-    return html_liste_livres
-
-
-def result_html():
-    logging.info('Sort info en HTML.')
-    result="""<!DOCTYPE html>
-        <html lang="en">
-        <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <center>
-        <title>library</title>
-        </head>
-        <h3>Voici la liste des livres présents dans la base de données</h3>
-        <body>
-        <style>
-        ul
-        {
-            line-height:70px;
-        }
-        </style>
-        <div>
-            <ul>"""+liste_html()+"""</ul>
-        </div>
-        <div>Made by Djan, Emilie, Sarah, Stephanie</div>
-        </body>
-        </center>
-        </html>"""
-    return result
-
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    """ Cette fonction permet de faire la liaison avec
+        le document index.html et de générer une page
+        HTML.
+    """
     logging.info('Python HTTP trigger function processed a request.')
-    return func.HttpResponse(
-        result_html(), status_code=200, mimetype='text/html'
-    )
+    with open('index.html') as file_:
+        template = Template(file_.read())
+    logging.info('connexion index.html')
+    result = template.render(dico_livre = listelivre())
+    return func.HttpResponse(result, status_code=200, mimetype="text/html")
