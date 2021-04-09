97384ef36bfccabf823dfacd7c7499ff919c7376
emilie-lb <62951146+emilie-lb@users.noreply.github.com>
2021-04-09 14:44:09 +0200

    fichier pour creer la base de données

---
+++ database_library.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS library;
+CREATE DATABASE library;
+USE library;
+
+-- créer une table clients
+DROP TABLE IF EXISTS liste_livres;
+CREATE TABLE liste_livres(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT UNIQUE,
+    titre VARCHAR(255),
+    infos VARCHAR(255),
+    url_blob VARCHAR(255),
+    nombre_par_mot VARCHAR(255)
+);
+-- SELECT * FROM library.liste_livres;
